640d70472cbd4fa898cb96d89941cd5ede2988f8
Michael <michael.r.truscott@gmail.com>
2022-03-13 15:39:53 +1100

    Add empty NetServerChessGameScene

---
+++ Chess/NetServerChessGameScene.cpp
@@ -0,0 +1,1 @@
+#include "NetServerChessGameScene.h"
---
+++ Chess/NetServerChessGameScene.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "ChessGameScene.h"
+
+// TODO: extend ChessGameScene to work with a network-connected client playing the other player's moves
+// e.g.
+// - Wait until client connects before starting game
+// - On client connection, send back a TCP message to assign them to the white/black side (at random?)
+// - Show message box with failure if client disconnects
+// - On making a move, transmit the move over the client TCP connection
+// - On receiving a move from the client, check it's a legal move, if so, apply it to the board state and send a MoveAck back
+class NetServerChessGameScene : public ChessGameScene
+{
+};
+
